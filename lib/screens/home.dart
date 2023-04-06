ff1f1cbf2b5e2599e3a2bbd98483ef828fe52c04
jhoisz <jvrda@ufpi.edu.br>
2023-04-06 19:24:11 -0300

    style: criei um theme e coloquei padding na Home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,27 +3,36 @@ import 'package:panucci_movies/components/movie_card.dart';
 import 'package:panucci_movies/components/search_input.dart';
 
 class Home extends StatelessWidget {
-Home({ Key? key }) : super(key: key);
-final TextEditingController inputController = TextEditingController();
+  Home({Key? key}) : super(key: key);
+  final TextEditingController inputController = TextEditingController();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(child: SearchInput(searchTextController: inputController),),
-          SliverToBoxAdapter(
-            child: Text("Movies"),
-          ),
-          SliverGrid.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, childAspectRatio: 0.7, crossAxisSpacing: 16),
-              itemBuilder: (context, index) => MovieCard(),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CustomScrollView(slivers: <Widget>[
+            SliverToBoxAdapter(
+              child: SearchInput(searchTextController: inputController),
+            ),
+            SliverToBoxAdapter(
+              child: Text(
+                "Filmes",
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+            ),
+            SliverGrid.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.7,
+                  crossAxisSpacing: 16),
+              itemBuilder: (context, index) => const MovieCard(),
               itemCount: 6,
             ),
-        ]
+          ]),
+        ),
       ),
-    ),);
+    );
   }
 }
