97d4a9935aa235df911a145e874f4b1ab35417ab
ikyrie <mah.c5r@gmail.com>
2023-04-11 15:13:24 -0300

    Transformando os nomes em portugues para ingles

--- lib/movies.dart
+++ lib/movies.dart
@@ -1,191 +1,191 @@
-import 'models/filme.dart';
+import 'models/movie.dart';
 
-List<Filme> listaFilmes = const [
-  Filme(
-    nome: "A Redenção",
-    classificacao: "18",
-    duracao: "01h 50min",
+List<Movie> moviesList = const [
+  Movie(
+    name: "A Redenção",
+    classification: "18",
+    duration: "01h 50min",
     sinopse:
         "Um ex-presidiário tenta se redimir com a família enquanto confronta um antigo companheiro de cela que busca vingança.",
-    genero: "Suspense",
-    imagemURI:
+    genre: "Suspense",
+    imageURI:
         "https://i.pinimg.com/originals/33/20/26/332026ad579079ffc4f3dba3c2cb15f6.jpg",
-    horarios: ["14:00", "16:30", "21:30"],
+    sessions: ["14:00", "16:30", "21:30"],
   ),
-  Filme(
-    nome: "Abrace a Sua Sombra",
-    classificacao: "16",
-    duracao: "01h 55min",
+  Movie(
+    name: "Abrace a Sua Sombra",
+    classification: "16",
+    duration: "01h 55min",
     sinopse:
         "Um homem descobre um segredo sombrio sobre seu passado e precisa enfrentar as consequências para proteger a si mesmo e a sua família.",
-    genero: "Drama",
-    imagemURI:
+    genre: "Drama",
+    imageURI:
         "https://www.denofgeek.com/wp-content/uploads/2019/11/dark_waters.jpg?resize=768%2C432",
-    horarios: ["13:00", "15:45", "18:20", "20:50"],
+    sessions: ["13:00", "15:45", "18:20", "20:50"],
   ),
-  Filme(
-    nome: "O Mistério no Museu",
-    classificacao: "12",
-    duracao: "01h 47min",
+  Movie(
+    name: "O Mistério no Museu",
+    classification: "12",
+    duration: "01h 47min",
     sinopse:
         "Um grupo de amigos visita um museu durante uma noite de sono e se depara com um mistério aterrador.",
-    genero: "Terror",
-    imagemURI:
+    genre: "Terror",
+    imageURI:
         "https://i.pinimg.com/originals/53/61/b9/5361b9bb6fd45de6ffb7b0b0eeb260c6.jpg",
-    horarios: ["17:30", "20:00", "22:15"],
+    sessions: ["17:30", "20:00", "22:15"],
   ),
-  Filme(
-    nome: "Toque de Recolher",
-    classificacao: "14",
-    duracao: "02h 10min",
+  Movie(
+    name: "Toque de Recolher",
+    classification: "14",
+    duration: "02h 10min",
     sinopse:
         "Em um futuro próximo, um governo autoritário estabelece um toque de recolher para conter distúrbios da população. Um grupo de jovens se junta para enfrentar a opressão e lutar pela liberdade.",
-    genero: "Suspense",
-    imagemURI:
+    genre: "Suspense",
+    imageURI:
         "https://www.1zoom.me/big2/800/286110-Sepiaphy_384044.jpg",
-    horarios: ["15:00", "18:00", "21:00"],
+    sessions: ["15:00", "18:00", "21:00"],
   ),
-  Filme(
-    nome: "O Amor é Cego",
-    classificacao: "Livre",
-    duracao: "01h 37min",
+  Movie(
+    name: "O Amor é Cego",
+    classification: "Livre",
+    duration: "01h 37min",
     sinopse:
         "Uma comédia romântica sobre um jovem que não consegue ver a beleza interior de uma moça em virtude da sua aparência.",
-    genero: "Comédia",
-    imagemURI:
+    genre: "Comédia",
+    imageURI:
         "https://i.pinimg.com/originals/be/6b/91/be6b91f8b267d83e23c3b3f82a81cf8a.jpg",
-    horarios: ["12:00", "14:30", "19:00"],
+    sessions: ["12:00", "14:30", "19:00"],
   ),
-  Filme(
-    nome: "O Monstro da Floresta",
-    classificacao: "12",
-    duracao: "01h 58min",
+  Movie(
+    name: "O Monstro da Floresta",
+    classification: "12",
+    duration: "01h 58min",
     sinopse:
         "Um grupo de cientistas faz uma expedição na floresta amazônica em busca de uma planta medicinal, mas acaba encontrando algo muito mais perigoso.",
-    genero: "Terror",
-    imagemURI:
+    genre: "Terror",
+    imageURI:
         "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/25-best-netflix-horror-movies-2020-bird-box-1560774699.jpg",
-    horarios: ["15:20", "19:35", "22:10"],
+    sessions: ["15:20", "19:35", "22:10"],
   ),
-  Filme(
-    nome: 'Jogos Vorazes: A Esperança - O Final',
-    classificacao: '14',
-    duracao: '02h 17min',
+  Movie(
+    name: 'Jogos Vorazes: A Esperança - O Final',
+    classification: '14',
+    duration: '02h 17min',
     sinopse:
         'Após sobreviver duas vezes aos Jogos, Katniss Everdeen (Jennifer Lawrence) servirá como prova definitiva da rebelião contra a Capital. Enquanto se prepara para a batalha final, Katniss precisa decidir em quem confiar.',
-    genero: 'Drama',
-    imagemURI:
+    genre: 'Drama',
+    imageURI:
         'https://observatoriodocinema.uol.com.br/wp-content/uploads/2019/06/jogos-vorazes-a-esperana-parte-2.jpg',
-    horarios: ['10:00', '13:00', '16:00'],
+    sessions: ['10:00', '13:00', '16:00'],
   ),
-  Filme(
-    nome: 'A Origem',
-    classificacao: '14',
-    duracao: '02h 28min',
+  Movie(
+    name: 'A Origem',
+    classification: '14',
+    duration: '02h 28min',
     sinopse:
         'Dom Cobb (Leonardo DiCaprio) é um habilidoso ladrão, especializado em roubar segredos diretamente do inconsciente das pessoas durante o sono. Sua habilidade fez com que ele se tornasse um dos homens mais procurados do mundo.',
-    genero: 'Suspense',
-    imagemURI:
+    genre: 'Suspense',
+    imageURI:
         'https://br.web.img3.acsta.net/pictures/210/020/21002005_20130605134106469.jpg',
-    horarios: ['08:00', '13:00', '19:00'],
+    sessions: ['08:00', '13:00', '19:00'],
   ),
-  Filme(
-    nome: 'Bacurau',
-    classificacao: '16',
-    duracao: '02h 11min',
+  Movie(
+    name: 'Bacurau',
+    classification: '16',
+    duration: '02h 11min',
     sinopse:
         'Pouco após a morte de dona Carmelita, aos 94 anos, os moradores de um pequeno povoado localizado no sertão brasileiro, chamado Bacurau, descobrem que a comunidade não consta mais nos mapas oficiais.',
-    genero: 'Drama',
-    imagemURI:
+    genre: 'Drama',
+    imageURI:
         'https://www.buscafilmes.com.br/wp-content/uploads/2019/09/bacurau.jpg',
-    horarios: ['09:00', '14:00', '20:00'],
+    sessions: ['09:00', '14:00', '20:00'],
   ),
-  Filme(
-    nome: 'It: A Coisa',
-    classificacao: '16',
-    duracao: '02h 15min',
+  Movie(
+    name: 'It: A Coisa',
+    classification: '16',
+    duration: '02h 15min',
     sinopse:
         'Uma criatura sobrenatural aterroriza uma cidade no Maine e a cada 27 anos se alimenta das crianças locais.',
-    genero: 'Terror',
-    imagemURI:
+    genre: 'Terror',
+    imageURI:
         'https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg',
-    horarios: ['12:00', '16:00', '22:00'],
+    sessions: ['12:00', '16:00', '22:00'],
   ),
-  Filme(
-    nome: 'O Lobo de Wall Street',
-    classificacao: '18',
-    duracao: '03h 00min',
+  Movie(
+    name: 'O Lobo de Wall Street',
+    classification: '18',
+    duration: '03h 00min',
     sinopse:
         'Novato na Bolsa de Valores de Wall Street, Jordan Belfort (Leonardo DiCaprio) sonha com riqueza e poder, mas acaba mergulhando em meio a casos de corrupção e excessos.',
-    genero: 'Comédia',
-    imagemURI:
+    genre: 'Comédia',
+    imageURI:
         'https://i.pinimg.com/originals/7a/74/d0/7a74d05be1f08fd40cdbb60a16bac7cc.jpg',
-    horarios: ['11:00', '15:00', '21:00'],
+    sessions: ['11:00', '15:00', '21:00'],
   ),
-  Filme(
-    nome: "A Casa dos Sonhos",
-    classificacao: "16",
-    duracao: "01h 50min",
+  Movie(
+    name: "A Casa dos Sonhos",
+    classification: "16",
+    duration: "01h 50min",
     sinopse:
         "Um casal compra a casa dos sonhos, mas descobrem que ela já foi palco de um terrível assassinato.",
-    genero: "Suspense",
-    imagemURI:
+    genre: "Suspense",
+    imageURI:
         "https://i.pinimg.com/564x/be/65/df/be65df2094d7bf2feb2b36a7b122cde4.jpg",
-    horarios: ["10:00", "18:00", "22:30"],
+    sessions: ["10:00", "18:00", "22:30"],
   ),
-  Filme(
-    nome: "O Grande Roubo",
-    classificacao: "14",
-    duracao: "02h 25min",
+  Movie(
+    name: "O Grande Roubo",
+    classification: "14",
+    duration: "02h 25min",
     sinopse:
         "Um grupo de amigos decide fazer um roubo arriscado em um cassino para sair da vida medíocre em que vivem.",
-    genero: "Suspense",
-    imagemURI:
+    genre: "Suspense",
+    imageURI:
         "https://i.pinimg.com/564x/9d/c3/72/9dc372ce5d1d6f5c213f3880536ea548.jpg",
-    horarios: ["11:00", "17:30", "20:00"],
+    sessions: ["11:00", "17:30", "20:00"],
   ),
-  Filme(
-    nome: "Amor e Mentiras",
-    classificacao: "18",
-    duracao: "02h 10min",
+  Movie(
+    name: "Amor e Mentiras",
+    classification: "18",
+    duration: "02h 10min",
     sinopse:
         "Um casal vive um relacionamento conturbado, onde a mentira e a traição são constantes.",
-    genero: "Drama",
-    imagemURI:
+    genre: "Drama",
+    imageURI:
         "https://i.pinimg.com/564x/d9/23/fa/d923fa8f16b1f57097411a94c20446c7.jpg",
-    horarios: ["12:15", "15:45", "21:30"],
+    sessions: ["12:15", "15:45", "21:30"],
   ),
-  Filme(
-    nome: "A Noite do Medo",
-    classificacao: "16",
-    duracao: "01h 45min",
+  Movie(
+    name: "A Noite do Medo",
+    classification: "16",
+    duration: "01h 45min",
     sinopse:
         "Um grupo de amigos decide passar a noite em uma casa abandonada, mas o que deveria ser um divertimento se transforma em um pesadelo quando começam a acontecer coisas estranhas.",
-    genero: "Terror",
-    imagemURI:
+    genre: "Terror",
+    imageURI:
         "https://i.pinimg.com/564x/8a/e2/c2/8ae2c2c06d11f97332aabec3cd97e0b9.jpg",
-    horarios: ["13:30", "19:00", "23:15"],
+    sessions: ["13:30", "19:00", "23:15"],
   ),
-  Filme(
-    nome: "A Separada",
-    classificacao: "12",
-    duracao: "01h 55min",
+  Movie(
+    name: "A Separada",
+    classification: "12",
+    duration: "01h 55min",
     sinopse:
         "Uma mulher decide deixar o marido após anos de um relacionamento conturbado, mas a separação não será tão fácil quanto ela imaginava.",
-    genero: "Drama",
-    imagemURI:
+    genre: "Drama",
+    imageURI:
         "https://i.pinimg.com/564x/1f/2a/81/1f2a811dbe18198e20496101a73b7bdb.jpg",
-    horarios: ["14:00", "17:00", "20:30"],
+    sessions: ["14:00", "17:00", "20:30"],
   ),
-  Filme(
-    nome: "A Grande Mentira",
-    classificacao: "14",
-    duracao: "02h 10min",
+  Movie(
+    name: "A Grande Mentira",
+    classification: "14",
+    duration: "02h 10min",
     sinopse:
         "Um homem contrata um detetive particular para investigar a morte de sua esposa, mas tudo pode não passar de uma grande mentira.",
-    genero: "Suspense",
-    imagemURI:
+    genre: "Suspense",
+    imageURI:
         "https://i.pinimg.com/564x/e5/6e/b9/e56eb989f40db98dccb2502286fa8f49.jpg",
-    horarios: ["11:45", "16:30", "21:15"],
+    sessions: ["11:45", "16:30", "21:15"],
   ),
 ];
