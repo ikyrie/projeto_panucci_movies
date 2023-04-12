e948936dc9cffb8d970ed33e1d0706cc899a5692
ikyrie <mah.c5r@gmail.com>
2023-04-12 18:02:34 -0300

    Fazendo ajustes na tela de checkout; Removendo os analytics criados sem querer (sorry);

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class PanucciMovies extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: myTheme,
-      home: Home(),
+      home: Checkout(homeContext: context),
     );
   }
 }
--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -20,27 +20,37 @@ class Checkout extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.only(bottom: 8.0),
-                child: Row(children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.only(right: 16),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(image: AssetImage("assets/images/mario_filme.jpg"), fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(right: 16),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(image: AssetImage("assets/images/mario_filme.jpg"), fit: BoxFit.cover),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      width: 80,
+                      height: 120,
                     ),
-                    width: 80,
-                    height: 120,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text("Mario the movie", style: Theme.of(context).textTheme.bodyLarge),
-                        const ParentalRating(),
-                        const Text("18:00"),
-                      ],
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Mario the movie", style: Theme.of(context).textTheme.bodyLarge),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Row(
+                              children: [
+                                const ParentalRating(),
+                                Text('01h 32m', style: Theme.of(context).textTheme.displaySmall),
+                              ],
+                            ),
+                          ),
+                          const Text("Sessão: 18:00"),
+                        ],
+                      ),
                     ),
-                  ),
-                ],),
+                  ],),
               ),
               const Padding(
                 padding: EdgeInsets.only(bottom: 8.0),
