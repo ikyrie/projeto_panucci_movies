9b92cde580ef6e3bbc6b3ed54577da3e6675de14
jhoisz <jvrda@ufpi.edu.br>
2023-04-13 15:05:05 -0300

    Acrescentei no movel de Filme um construtor para transformar o JSON em objeto Filme.

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,10 +1,34 @@
 class Movie {
-  const Movie({required this.name, required this.classification, required this.duration, required this.sinopse, required this.genre, required this.imageURI, required this.sessions});
-    final String name;
-    final String classification;
-    final String duration;
-    final String sinopse;
-    final String genre;
-    final String imageURI;
-    final List<String> sessions;
+  Movie(
+      {required this.name,
+      required this.classification,
+      required this.duration,
+      required this.sinopse,
+      required this.genre,
+      required this.imageURI,
+      required this.sessions});
+  String? name;
+  String? classification;
+  String? duration;
+  String? sinopse;
+  String? genre;
+  String? imageURI;
+  List<String>? sessions;
+
+  Movie.fromJson(Map<String, dynamic> json) {
+    name = json['name'].toString();
+    classification = json['classification'].toString();
+    duration = json['duration'].toString();
+    sinopse = json['sinopse'].toString();
+    genre = json['genre'].toString();
+    imageURI = json['imageURI'].toString();
+    if (json['sessions'] != null) {
+      final v = json['sessions'];
+      final arr0 = <String>[];
+      v.forEach((v) {
+        arr0.add(v.toString());
+      });
+      sessions = arr0;
+    }
+  }
 }
