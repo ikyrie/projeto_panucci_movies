c4ca9c9d32d8ed00414eed60d9ef275217adaae4
jhoisz <jvrda@ufpi.edu.br>
2023-04-13 15:00:13 -0300

    Criei os arquivos home_cubit e home_state.

---
+++ lib/screens/home/cubit/home_state.dart
@@ -0,0 +1,20 @@
+part of 'home_cubit.dart';
+
+@immutable
+abstract class HomeState {}
+
+class HomeInitial extends HomeState {}
+
+class HomeLoading extends HomeState {}
+
+class HomeSuccess extends HomeState {
+  final List<Movie> movies;
+
+  HomeSuccess(this.movies);
+}
+
+class HomeError extends HomeState {
+  final String error;
+
+  HomeError(this.error);
+}
