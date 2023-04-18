c506a064c58b33ee1dc73d6d8d9c8620dfaf4578
ikyrie <mah.c5r@gmail.com>
2023-04-18 16:35:37 -0300

    Arrumando a cor amarela da classificação por conta do contraste

--- lib/components/classification.dart
+++ lib/components/classification.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 enum Classification {
   livre('Livre', Colors.green, 'L'),
   naoRecomendado10('10', Colors.blue, '10'),
-  naoRecomendado12('12', Colors.yellow, '12'),
+  naoRecomendado12('12', Color(0xFFF8DA00), '12'),
   naoRecomendado14('14', Colors.orange, '14'),
   naoRecomendado16('16', Colors.red, '16'),
   naoRecomendado18('18', Colors.black, '18');
